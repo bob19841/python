215c088b7a275ffa692f2f0e0f9b9b942660a78f
bob19841 <sub@kubik.tech>
2017-01-21 13:28:20 -0600

    Add files via upload
    compare a list of ad usernames to folders to find ones that are no longer employeed

---
+++ compare.py
@@ -0,0 +1,16 @@
+import os
+
+user_file = 'users.txt'
+with open(user_file) as f:
+    content = f.readlines()
+# remove carriage return
+content = [x.strip() for x in content]
+content = [x.lower() for x in content]
+print(content)
+
+root = 'x:'
+
+for subfolder in os.listdir(root):
+    if subfolder.lower() not in content:
+        new_line = 'This folder doesnt belong to an active citrix user "' + subfolder + '"'
+        print('This folder doesnt belong to an active citrix user  "' + subfolder + '"')
